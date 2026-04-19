b699a8a9843eb059d17d34d7100247a32a55841a
2nil <casalalanne@free.fr>
2026-04-19 19:41:06 +0200

    Checked in by dplal from PC-Denis on dim. 19 avr. 2026 19:41:06

--- src/app_info.h
+++
@@ -1,7 +0,0 @@
-#ifndef APP_INFO_H
-#define APP_INFO_H
-struct
-{
-  std::string name, version, copyright, decoration, commit, created_at, platform;
-} app_info = {"subadjust", "1.0.0", "Copyright © D. LALANNE - MIT License - No warranty of any kind.", "", "0f06a64", "2026-04-19T19:34:52Z", "Windows_NT_10.0.26200 x86_64"};
-#endif
