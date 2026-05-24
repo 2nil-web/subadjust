cd715ef293d60d5ed749d15eddb867e363ebf778
2nil <casalalanne@free.fr>
2026-05-24 19:44:32 +0200

    Checked in by dplal from PC-Denis on dim. 24 mai 2026 19:44:31

--- assets/gen_subs.cpp
+++ assets/gen_subs.cpp
@@ -1,4 +1,5 @@
 
+#include <filesystem>
 #include <iostream>
 #include <limits>
 #include <random>
@@ -15,7 +16,7 @@ std::string pad(unsigned int d, size_t np = 2)
 }
 
 // Convert a int in millisecond into a string of the format HH:MM:SS[.|,]SSS to
-std::string ms_to_str(int millisec, bool dot = true)
+std::string ms_to_str(int millisec, bool dot = false)
 {
   bool neg = false;
   if (millisec < 0)
@@ -47,44 +48,73 @@ std::string ms_to_str(int millisec, bool dot = true)
   return str;
 }
 
-int current_start = 0, n_subs = 1500, interval_average = 9400, interval_gap = 60000, length_average = 3000, length_gap = 10000;
-
-int rnd_max(int max)
+int rnd(int min, int max)
 {
   std::random_device r;
   std::default_random_engine e(r());
-  std::uniform_int_distribution<int> uniform_dist(0, max);
+  std::uniform_int_distribution<int> uniform_dist(min, max);
   return uniform_dist(e);
 }
 
-std::string a_subtitle(int n)
+int start, length, nsubs, ave_dur, curr_start, curr_end;
+std::string a_subtitle(int n, std::string s = "Unimportant blah blah...")
 {
-  int length = 300 + rnd_max(length_average) + rnd_max(length_gap);
-  int current_end = current_start + length;
-  std::string res = std::to_string(n + 1) + '\n' + ms_to_str(current_start) + " --> " + ms_to_str(current_end) + "\nToujours les même mots, on s\'en fout ...\n";
-  current_start += 300 + length + rnd_max(interval_average) + rnd_max(interval_gap);
-  return res;
+  curr_end = curr_start + rnd(ave_dur - rnd(ave_dur / 10, 5 * ave_dur / 10), ave_dur + rnd(ave_dur / 10, 5 * ave_dur / 10));
+  std::string ret = std::to_string(n + 1) + '\n' + ms_to_str(curr_start) + " --> " + ms_to_str(curr_end) + "\n" + s + "\n";
+  curr_start = curr_end + rnd(ave_dur - rnd(ave_dur / 10, 5 * ave_dur / 10), ave_dur + rnd(ave_dur / 10, 5 * ave_dur / 10));
+
+  return ret;
 }
 
+/*
+./gen_subs.exe >sub.srt
+./gen_subs.exe 94500 5243000 1678 >sub.srt
+*/
 int main(int argc, char **argv)
 {
   std::vector<std::string> args(argv, argv + argc);
 
-  if (args.size() > 1)
-    current_start = std::stoi(args[1]);
-  if (args.size() > 2)
-    current_start = std::stoi(args[2]);
-  if (args.size() > 3)
-    interval_average = std::stoi(args[3]);
-  if (args.size() > 4)
-    interval_gap = std::stoi(args[4]);
-  if (args.size() > 5)
-    length_average = std::stoi(args[5]);
-  if (args.size() > 6)
-    length_gap = std::stoi(args[6]);
-
-  for (int i = 0; i < n_subs; i++)
+  if (args[1] == "-h" || args[1] == "-help" || args[1] == "--help")
   {
-    std::cout << a_subtitle(i) << std::endl;
+    std::cout << "Usage: " << std::filesystem::path(args[0]).stem().string() << " [ARGUMENTS]";
+    std::cout << R"EOF(
+Generate a subtitle file in srt format.
+Arguments must respect the following order and are in millisecond when about time:
+Start time (default from 0 to 2 minutes).
+Duration of subtitles (default from 30 minutes to 3 hours).
+Count of subtitles in the file (default from 400 to 2000).
+)EOF";
+  }
+  else
+  {
+    if (args.size() > 1)
+      start = std::stoi(args[1]);
+    // Sinon entre 0 et 2 minutes (en ms)
+    else
+      start = rnd(0, 120000);
+
+    curr_start = start;
+
+    if (args.size() > 2)
+      length = std::stoi(args[2]);
+    // Sinon entre 30 minutes et 3 heures (en ms)
+    else
+      length = rnd(1800000, 10800000);
+
+    if (args.size() > 3)
+      nsubs = std::stoi(args[3]);
+    // Sinon entre 400 et 2000 sous-titres
+    else
+      nsubs = rnd(400, 2000);
+
+    ave_dur = length / (2 * nsubs);
+
+    int i;
+    for (i = 0; i < nsubs - 1; i++)
+    {
+      std::cout << a_subtitle(i) << std::endl;
+    }
+
+    std::cout << a_subtitle(i, "Stats:") << "  Start = " << ms_to_str(start) << "\n  Duration = " << ms_to_str(curr_end - start) << "\n  Subtitle count = " << nsubs << "\n  Average duration of a subtitle = " << ms_to_str(ave_dur) << std::endl;
   }
 }
