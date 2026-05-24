17b7680a5e1fd7f5366f897a1c7bfa30c47e48de
denis <denis.lalanne@laposte.net>
2026-05-24 16:18:30 +0200

    Checked in by root with git user denis and git email denis.lalanne@laposte.net from home.dplalanne.fr on dim. 24 mai 2026 16:18:30 CEST

--- assets/subtitle_stat.sh
+++ assets/subtitle_stat.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
+TZ=utc
+
+function hms_to_ms () {
+  IFS=':' read -r h m ms <<<"$*"
+  [[ $h =~ ^0*(.+)$ ]] && h=${BASH_REMATCH[1]}
+  [[ $m =~ ^0*(.+)$ ]] && m=${BASH_REMATCH[1]}
+  [[ $ms =~ ^0*(.+)$ ]] && ms=${BASH_REMATCH[1]}
+
+  echo $((h*3600000+m*60000+ms))
+}
+
+echo "num;start;end;dur;dur/sub;file"
 for i in *.srt
 do
-  ns=$(dos2unix -q -O "$i" | grep -E ^[0-9]+$ | wc -l)
-  echo "$ns $i"
+  tss=$(dos2unix -q -O "$i" | sed -n 's/^\(..:..:..\),\(...\).*-->.*\(..:..:..\),\(...\).*$/\1\2 \3\4/p')
+  n=$(wc -l <<<"$tss")
+  read ts1 te1 ts2 te2 <<<$(sed '1b;$b;d' <<<"$tss" | tr '\n' ' ')
+  ms1=$(hms_to_ms "$ts1")
+  ms2=$(hms_to_ms "$te2")
+  let d=ms2-ms1
+  let ds=d/n
+  echo "$n;$ms1;$ms2;$d;$ds;$i"
 done
